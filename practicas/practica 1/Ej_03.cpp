566b2b6d7793a1344ce1178222da8a7009106e95
Ignacio Fernandez Oromendia <ignaciooromendia@gmail.com>
2023-08-16 10:16:53 -0300

    Ej 03 opt

--- practicas/practica 1/Ej_03.cpp
+++ practicas/practica 1/Ej_03.cpp
@@ -6,23 +6,41 @@ using namespace std;
 int n;
 vector<vector<int> > M;
 vector<int> I;
+vector<int> maxs_fila;
 int max_sum = -1;
 
-void max_mat_sum(int k, int i, int sum, vector<int>& indices) {
+int suma(vector<int>& indices, int i) {
+    int res = 0;
+    for(int j = 0; j < indices.size(); j++) res += M[i][indices[j]-1];
+    return res *= 2;
+}
+
+void max_mat_sum(int k, int i, int sum, vector<int>& indices, int max_alcanzable) {
     if (k == 0) {
         if(sum > max_sum) {
             I = indices;
             max_sum = sum;
         }
     } else if (i < M.size()){
-        indices.push_back(i+1);
-        int new_sum = 0;
-        for(int j = 0; j < indices.size(); j++) new_sum += M[i][indices[j]-1];
-        new_sum *= 2;
-        max_mat_sum(k - 1, i + 1, sum + new_sum, indices);
-        indices.pop_back();
-        max_mat_sum(k, i + 1, sum, indices);
+        if (max_alcanzable > max_sum) {
+            indices.push_back(i+1);
+            int new_sum = suma(indices, i);
+
+            max_mat_sum(k - 1, i + 1, sum + new_sum, indices, max_alcanzable - maxs_fila[i]);
+            indices.pop_back();
+            max_mat_sum(k, i + 1, sum, indices, max_alcanzable - maxs_fila[i]);
+
+        }
     }
+}
+
+int max_vector_sim(vector<int>& v, int j) {
+    int m = 0;
+    for(int i = j; i < v.size(); i++) {
+        if (v[i] > m) m = v[i];
+    }
+    maxs_fila.push_back(m);
+    return m;
 }
 
 int main() {
@@ -44,18 +62,27 @@ int main() {
         M.push_back(fila);
     }
 
-    max_mat_sum(k, 0, 0, indices);
-    cout << max_sum << endl;
+    int max_alcanzable = 0;
+    for(int i = 0; i < M.size(); i++) {
+        int max_fila = max_vector_sim(M[i], i+1);
+        max_alcanzable += max_fila;
+    }
+
+    max_mat_sum(k, 0, 0, indices, max_alcanzable);
+
+    cout << "Maximo: " << max_sum << endl;
+
+    cout << "I = { ";
 
     for(int i = 0; i < I.size(); i++) cout << I[i] << " ";
-    cout << endl;
+    cout << "}" << endl;
 
 }
 
 /*
 4 3            
-0 0 10 1
-0 0 5 2
+0 10 10 1
+10 0 5 2
 10 5 0 1
 1 2 1 0
 */
