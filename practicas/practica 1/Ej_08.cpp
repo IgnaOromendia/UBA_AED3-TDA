a46914527b71a01f9b841448dc9a69150ce7c09c
Ignacio Fernandez Oromendia <ignaciooromendia@gmail.com>
2023-08-19 16:52:16 -0300

    Ej_08 sin bottom up

--- practicas/practica 1/Ej_08.cpp
+++ practicas/practica 1/Ej_08.cpp
@@ -9,36 +9,40 @@ vector<vector<int> > memo;
 vector<bool> cortes;
 
 // Top Down
-int corte(int i, int j) {
+int min_costo(int i, int j) {
     if (i+1 == j) return 0;
-    if(memo[i][j] == -1) {
-        int costo = cortes.size();
+    if (memo[i][j] == -1) {
+        int costo = cortes.size(); // corte más largo
+        bool hay_corte = false;
         for(int q = i + 1; q < j; q++) {
-            costo = min(costo, corte(i,q) + corte(q,j));
+            // En caso de que el las posicion sea de un corte caluclo el minimo
+            if (cortes[q]) {
+                costo = min(costo, min_costo(i, q) + min_costo(q, j));
+                hay_corte = true;
+            }
         }
-        memo[i][j] = j - i + costo;
+        if (hay_corte) memo[i][j] = costo + j - i;
+        else memo[i][j] = 0;
     }
     return memo[i][j];
 }
 
 // Bottom Up
-int corte_bu(int l) {
-    vector<vector<int> > M(l+1, vector<int>(l+1,l));
 
+int min_costo_bu(int l) {
+    vector<vector<int> > M(l+1, vector<int>(l+1, -1));
+
+    // Hacer un corte sin longitud
     M[0][0] = 0;
 
-    for(int i = 1; i < l; i++) {
-        for (int j = 1; j < l; j++) {
-            int costo = cortes.size();
-            for(int q = i + 1; q < j; q++) {
-                costo = min(costo, M[i][q] + M[q][j]);
-            }
-            M[i][j] = j - i + costo;
+    for (int i = 0; i <= l; i++) {
+        for(int j = i; j <= l; j++) {
+            
         }
     }
 
-    return M[l][l];
 }
+
 
 int main() {
     int l, c; cin >> l >> c;
@@ -51,13 +55,13 @@ int main() {
         cortes[e] = true;
     }
 
-    int coste = corte(0,l);
+    int coste = min_costo(0,l);
 
     cout << "Coste de los cortes (TD): " << coste << endl;
 
-    coste = corte_bu(l);
+    /*coste = corte_bu(l);
 
-    cout << "Coste de los cortes (BU): " << coste << endl;
+    cout << "Coste de los cortes (BU): " << coste << endl;*/
 
 
 
