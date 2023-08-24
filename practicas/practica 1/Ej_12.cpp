1c8c43aabd155400d71077898e055a8c7ee7408f
Ignacio <ignaciooromendia@gmail.com>
2023-08-23 21:37:18 -0300

    Agrega poda

--- practicas/practica 1/Ej_12.cpp
+++ practicas/practica 1/Ej_12.cpp
@@ -9,6 +9,7 @@ int caras;
 // Escenario A (dados distinguibles)
 
 int suma_dados_d(int n, int s) {
+    if (s < 0) return 0;
     if(n == 0) return s == 0;
     if (memo[n][s] == -1) {
         int total = 0;
@@ -28,10 +29,17 @@ int suma_dados_i(int n, int s, int k) {
 
 int main() {
     int n, s; cin >> n >> caras >> s;
+    if (caras * n < s) {
+        memo = vector<vector<int> >(n+1, vector<int>(s+1, -1));
 
-    memo = vector<vector<int> >(n+1, vector<int>(s+1, -1));
+        int posibilidades = suma_dados_d(n,s);
+        cout << "Posibilidades de sumar " << s << " con " << n << " dados distinguibles de " << caras << " caras es "; 
+        cout << posibilidades << endl;
 
-    int posibilidades = suma_dados_d(n,s);
-
-    cout << "Posibilidades de sumar " << s << " con " << n << " dados distinguibles de " << caras << " caras es " << posibilidades << endl;
+        posibilidades = suma_dados_i(n,s,caras);
+        cout << "Posibilidades de sumar " << s << " con " << n << " dados indistinguibles de " << caras << " caras es "; 
+        cout << posibilidades << endl;
+    } else {
+        cout << "No hay solución" << endl;
+    }
 }
