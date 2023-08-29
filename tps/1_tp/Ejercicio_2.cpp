75a11382853999370699fb75d706857912be5bcb
Ignacio <ignaciooromendia@gmail.com>
2023-08-29 10:04:36 -0300

    Ejercicio 2 falta terminar

--- tps/1_tp/Ejercicio_2.cpp
+++ tps/1_tp/Ejercicio_2.cpp
@@ -2,14 +2,27 @@
 #include<vector>
 
 using namespace std;
+using minuto = int;
+using costo = int;
+using parcial = pair<minuto, costo>;
 
-int n, min_d;
-vector<int> minutos;
-vector<int> descontento;
+const int INF = 1e9;
 
-int minimo_descontento(int i, int m) {
+int n;
+vector<parcial> parciales;
+vector<minuto> minutos;
+vector<costo> descontentos;
+
+int minimo_descontento(int i, int m, int c) {
+    if (i == n and c != n) return INF;
     if (i == n) return 0;
-    return min(minimo_descontento(i + 1, m + 1), minimo_descontento(i + 1, m + 1));
+    int corregir = parciales[i].second * (m + parciales[i].first) + minimo_descontento(i + 1, m + parciales[i].first, c + 1);
+    int siguiente = minimo_descontento(i + 1, m, c);
+    return min(corregir, siguiente);
+}
+
+bool comp(parcial p1, parcial p2) {
+    return p1.second > p2.second;
 }
 
 int main() {
@@ -18,18 +31,44 @@ int main() {
         cin >> n;
         int i = 0;
 
-        while(i < 2*n) {
+        minutos      = vector<minuto>(n);
+        descontentos = vector<costo>(n);
+        parciales    = vector<parcial>(n);
+
+        while(i < n) {
             int e; cin >> e;
-            if(i < n) minutos.push_back(e);
-            else descontento.push_back(e);
+            minutos[i] = e;
             i++;
         }
+
+        i = 0;
+        while(i < n) {
+            int e; cin >> e;
+            descontentos[i] = e;
+            i++;
+        }
+
+        i = 0;
+        while(i < n) {
+            //cout << minutos[i] << "-" << descontentos[i] << endl;
+            parciales[i] = make_pair(minutos[i], descontentos[i]);
+            i++;
+        }
+
+        sort(parciales.begin(), parciales.end(), comp);
+
+        int descontento = minimo_descontento(0,0,0);
+
+        cout << descontento << endl;
     }
 }
 
 /*
-1
+2
 3
 1 2 3
 2 2 2
+4
+2 1 3 2
+1 2 2 3
 */
