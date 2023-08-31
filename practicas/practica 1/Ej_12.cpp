ea7d39be20c076ea2bcaaa25531a56ac45b51394
Ignacio <ignaciooromendia@gmail.com>
2023-08-31 15:55:51 -0300

    Ejercicio 12 (Falta indistinguibles)

--- practicas/practica 1/Ej_12.cpp
+++ practicas/practica 1/Ej_12.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 vector<vector<int> > memo_d;
-vector<vector<int> > memo_i;
+vector<vector<vector<int> > > memo_i;
 int caras;
 
 // Escenario A (dados distinguibles)
@@ -14,9 +14,10 @@ int suma_dados_d(int n, int s) {
     if(n == 0) return s == 0;
     if (memo_d[n][s] == -1) {
         int total = 0;
-        for(int i = 1; i <= caras; i++) {
+
+        for(int i = 1; i <= caras; i++) 
             total += suma_dados_d(n - 1, s - i);
-        }
+
         memo_d[n][s] = total;
     }
     return memo_d[n][s];
@@ -25,18 +26,22 @@ int suma_dados_d(int n, int s) {
 // Escenario B (dados indistinguibles)
 
 int suma_dados_i(int n, int s, int k) {
+    // Si s es negativo es invalido
     if (s < 0) return 0;
+
+    // Si no quedan más dados tenemos que chequear la suma
     if (n == 0) return s == 0;
-    if (memo_i[n][k] == -1) {
+
+    // Chequea memoria
+    if (memo_i[n][s][k] == -1) {
         int total = 0;
-        for(int i = 1; i <= n; i++) {
-            for(int j = k; j > 0; j--) {
-                total += suma_dados_i(n - i, s - (k * i), j);
-            }
+        for(int i = 0; i < n; i++) {
+            total += suma_dados_i(n - i, s - (k * i), k - 1);
         }
-        memo_i[n][k] = total;
+        memo_i[n][s][k] = total;
     }
-    return memo_i[n][k];
+    
+    return memo_i[n][s][k];
 }
 
 int main() {
@@ -46,7 +51,7 @@ int main() {
    caras = 4;
     if (caras * n > s) {
         memo_d = vector<vector<int> >(n+1, vector<int>(s+1, -1));
-        memo_i = vector<vector<int> >(n+1, vector<int>(caras+1, -1));
+        memo_i = vector<vector<vector<int> > >(n+1, vector<vector<int> >(s+1, vector<int>(caras+1, -1)));
 
         int posibilidades = suma_dados_d(n,s);
         cout << "Posibilidades de sumar " << s << " con " << n << " dados distinguibles de " << caras << " caras es "; 
@@ -55,13 +60,6 @@ int main() {
         posibilidades = suma_dados_i(n,s,caras);
         cout << "Posibilidades de sumar " << s << " con " << n << " dados indistinguibles de " << caras << " caras es "; 
         cout << posibilidades << endl;
-
-        for(int i = 0; i < n+1; i++) {
-            for(int j = 0; j < caras+1; j++) {
-                cout << memo_i[i][j] << " ";
-            }
-            cout << endl;
-        }
 
     } else {
         cout << "No hay solución" << endl;
