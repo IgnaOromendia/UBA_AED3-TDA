1ae0092d0765e7fd7c2c730f83f75557472268b1
Ignacio <ignaciooromendia@gmail.com>
2023-09-02 14:18:22 -0300

    Ejercicio 4 con PD
    Falta reconstruir la solucion

--- tps/1_tp/Ejercicio_4.cpp
+++ tps/1_tp/Ejercicio_4.cpp
@@ -8,52 +8,76 @@ const int INF = 1e9;
 int n, cant_prov;
 vector<int> puestos;
 vector<int> proveedurias;
+vector<vector<vector<int> > > memo;
 
-// O(nk)
-int costo() {
+
+/// @param d desde que puesto calcular
+/// @return el costo de los puestos que estan adelante de la proveeduria d
+int costos_restantes(int d) {
     int suma = 0;
-    for(int i = 0; i < n; i++) {
-        int min = INF;
-        for(int j = 0; j < cant_prov; j++) {
-            int dist = abs(puestos[i] - proveedurias[j]);
-            if (dist < min) min = dist;
-        }
-        suma += min;
+    for(int i = d + 1; i < n; i++) {
+        suma += abs(puestos[i] - puestos[d]);
     }
     return suma;
 }
 
-int min_costo(int i, int k) {
-    if (i <= n and k == 0)  return costo();
+/// @param d Desde este puesto
+/// @param h Hasta este puesto
+/// @return los costos minimos de las distancias de los puestsos entre las proveedurias d y h
+int calcular_costos(int d, int h) {
+    int suma = 0;
+    for(int i = d + 1; i < h; i++) {
+        if (d == -1) suma += abs(puestos[i] - puestos[h]);
+        else suma += min(abs(puestos[i] - puestos[d]), abs(puestos[i] - puestos[h]));
+    }
+    return suma;
+}
+
+/// @param i el i-esimo puesto
+/// @param k la cantidad restante de proveedurias
+/// @param u la posicion de la última proveeduria
+/// @return el minimo costo de poner las k proveeduras
+int min_costo(int i, int k, int u) {
+    if (i <= n and k == 0)  return costos_restantes(u);
     if (i == n and k > 0)   return INF;
-    
-    proveedurias[k-1] = puestos[i];
-    int poner =   min_costo(i + 1, k - 1);
 
-    proveedurias[k-1] = INF;
-    int saltear = min_costo(i + 1, k);
+    if (memo[i][k-1][u] == -1) {
+        int poner = calcular_costos(u, i) + min_costo(i + 1, k - 1, i);
+        int saltear = min_costo(i + 1, k, u);
+        memo[i][k-1][u] = min(poner, saltear);
+    }
 
-    if (poner < saltear) proveedurias[k-1] = puestos[i];
-    
-    return min(poner, saltear);
+    return memo[i][k-1][u];
+}
+
+void reconstruir_solucion() {
+    // TODO
 }
 
 int main() {
-    int c;cin >> c;
+    int c = 1;//cin >> c;
     while(c--) {
-        cin >> n >> cant_prov;
+        // cin >> n >> cant_prov;
+        cant_prov = 2;
+        n = 4;
 
         puestos      = vector<int>(n);
         proveedurias = vector<int>(cant_prov);
+        memo = vector<vector<vector<int> > >(n, vector<vector<int> >(cant_prov, vector<int>(n, -1)));
         
-        int i = 0;
+        int i = n;
         while(i < n) {
             int e; cin >> e;
             puestos[i] = e;
             i++;
         }
 
-        int costo = min_costo(0,cant_prov);
+        puestos[0] = 1;
+        puestos[1] = 5;
+        puestos[2] = 15;
+        puestos[3] = 20;
+
+        int costo = min_costo(0,cant_prov, -1);
         
         cout << costo << endl;
         for(int j = cant_prov - 1; j >= 0; j--) cout << proveedurias[j] << " ";
