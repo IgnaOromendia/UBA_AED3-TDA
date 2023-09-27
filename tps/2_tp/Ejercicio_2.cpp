9f92a00df088f635691dd367d78792f1acd57a2a
Ignacio <ignaciooromendia@gmail.com>
2023-09-27 13:55:09 -0300

    Limpia (wrong awnser)

--- tps/2_tp/Ejercicio_2.cpp
+++ tps/2_tp/Ejercicio_2.cpp
@@ -26,12 +26,12 @@ Algoritmo
 - BFS desde H guardando la distancia a cada nodo                O(n + m)
 - Calcular camino a P:                                          O(n + m)
     Si min de manifestaciones < camino_P:                       
-        Eliminar todos los nodos tq camino_P < manifestacion_i
+        Eliminar todos los nodos tq tiempo_hasta_manifiestacion_i > manifestacion_i
         BFS desde H guardando la distancia a cada nodo
 - BFS desde P guardando la distancia a cada nodo                O(n + m)  
 - Calcular camino a H:                                          O(n + m)
     Si min de manifestaciones < camino_H:
-        Eliminar todos los nodos tq camino_H < manifestacion_i
+        Eliminar todos los nodos tq tiempo_hasta_manifiestacion_i + tiempo_hasta_p > manifestacion_i
         BFS desde P guardando la distancia a cada nodo
 */
 
@@ -47,18 +47,20 @@ struct manifestacion {
 };
 
 // Variables globales
-int n, m, P, H, camino_H, camino_P;
-manifestacion proxima_manifestacion;
+int n, m, P, H, tiempo_hasta_H, tiempo_hasta_P;
 vector<bool> marcados;
-vector<int> tiempo_H, tiempo_P;
+vector<int> tiempos;
 vector<manifestacion> manifestaciones;
 vector<vector<int> > adyacentes;
 
-// Complejidad: O(n + m)
-void bfs(int s, vector<int> &distancia) {
+// n = #cantidad de nodos
+
+// Complejidad: O(n)
+void bfs(int s) {
     vector<bool> visitado(n*m, false);
+    tiempos = vector<int>(n*m, -1);
     visitado[s] = true;
-    distancia[s] = 0;
+    tiempos[s] = 0;
     queue<int> cola;
     cola.push(s);
 
@@ -69,52 +71,35 @@ void bfs(int s, vector<int> &distancia) {
             // Si no fue marcado ni fue visitado, lo visitamos
             if(not visitado[w] and not marcados[w]) {
                 visitado[w] = true;
-                distancia[w] = distancia[v] + 1;
+                tiempos[w] = tiempos[v] + 1;
                 cola.push(w);
             }
         }
     }
 }
 
-// Complejidad: O(n + m)
-int calcular_camino_hasta_P() {
-    // En caso de que el camino sea más rápido que la primer manifestación lo devolvemos
-    if (tiempo_H[P] < proxima_manifestacion.tiempo) return tiempo_H[P];
-    
-    // Caso contrario, limpiamos el grafo de manifestaciones y volvemos a calcular el camino
+// Complejidad: O(n)
+void eliminar_nodos_manifestaciones(int t) {
     for(int i = 0; i < manifestaciones.size(); i++) {
-        marcados[manifestaciones[i].nodo] = manifestaciones[i].tiempo <= tiempo_H[P];
-    }
-        
-    // Reinicamos los caminos debido a las manifestaciones
-    tiempo_H = vector<int>(n*m, -1);
-
-    bfs(H, tiempo_H);
-    return tiempo_H[P];
-}
-
-// Complejidad: O(n + m)
-int calcular_camino_hasta_H() {
-    // En caso de que el camino sea más rápido que la primer manifestación lo devolvemos
-    if (tiempo_P[H] + tiempo_H[P] < proxima_manifestacion.tiempo) return tiempo_P[H];
-    
-    // Caso contrario, limpiamos el grafo de manifestaciones y volvemos a calcular el camino
-    for(int i = 0; i < manifestaciones.size(); i++) {
-        marcados[manifestaciones[i].nodo] = manifestaciones[i].tiempo <= (tiempo_P[manifestaciones[i].nodo] + tiempo_H[P]);
-    }
-
-    // Reinicamos los caminos debido a las manifestaciones
-    tiempo_P = vector<int>(n*m, -1);
-
-    bfs(P, tiempo_P);
-    return tiempo_P[H];
+        // Si el tiempo a llegar a la manifestacion es mayor o igual al tiempo en el que inicia entonces marcamos el nodo
+        if (not marcados[manifestaciones[i].nodo])
+            marcados[manifestaciones[i].nodo] = manifestaciones[i].tiempo <= (tiempos[manifestaciones[i].nodo] + t);
+    } 
 }
 
 // Complejidad: O(n)
-manifestacion proxima_manifestacion_para(int t) {
-    int i = 0;
-    while(manifestaciones[i].tiempo > t and i < manifestaciones.size()) i++;
-    return manifestaciones[i];
+void calcular_tiempos_desde(int v, int t) {
+    // Para obtener las distancias iniciales
+    bfs(v);
+
+    // Limpiamos el grafo de manifestaciones
+    eliminar_nodos_manifestaciones(t);
+
+    // Caluclamos los tiempos contando las manifestaciones
+    bfs(v);
+
+    if (v == H) tiempo_hasta_P = tiempos[P];
+    else tiempo_hasta_H = tiempos[H];
 }
 
 bool comp_manifestaciones(manifestacion a, manifestacion b) {
@@ -127,9 +112,8 @@ int main() {
         cin >> n >> m;
 
         // Inicializamos las variables
-        marcados = vector<bool>(n*m, false);
-        tiempo_H = vector<int>(n*m, -1);
-        tiempo_P = vector<int>(n*m, -1);
+        marcados   = vector<bool>(n*m, false);
+        tiempos    = vector<int>(n*m, -1);
         adyacentes = vector<vector<int> >(n*m, vector<int>());
 
         // Generamos el grafo
@@ -155,83 +139,28 @@ int main() {
 
         // Leemos cooredenada de H y P
         int x,y;
-        cin >> x >> y; H = x + y;
-        cin >> x >> y; P = x + y;
-        /*
-        // Imprimimos el grafo
-        cout << "G:" << endl;
-        for(int v = 0; v < n*m; v++) {
-             cout << v << ": ";
-             for(int w : adyacentes[v]) cout << w << " ";
-             cout << endl;
-        }
+        cin >> x >> y; H = (x * n) + y;
+        cin >> x >> y; P = (x * n) + y;
 
-        // Imprimimos las manifestaciones
-        
-        cout << "Manifestaciones: " << endl;
-        for(int i = 0; i < manifestaciones.size(); i++)
-           cout << "v: " << manifestaciones[i].nodo << ", t: " << manifestaciones[i].tiempo << endl;
-        
-        cout << "H: " << H << endl;
-        cout << "P: " << P << endl;*/
-
+        // cout << P << endl;
 
         // Ordenamos las manifestaciones por tiempo
         sort(manifestaciones.begin(), manifestaciones.end(), comp_manifestaciones);
 
         // Calculamos el camino a P
-        bfs(H, tiempo_H);
+        calcular_tiempos_desde(H, 0);
 
-        /*
-        // Mostramos los caminos desde H
-        cout << "Caminos desde H antes de calcular:" << endl;
-        for(int i = 0; i < n*m; i++)
-            cout << "v: " << i << ", t:" << tiempo_H[i] << endl;
-        */
+        // // Guardamos el camino hasta P
+        calcular_tiempos_desde(P, tiempo_hasta_P);
 
-        proxima_manifestacion = proxima_manifestacion_para(tiempo_H[P]);
+        // Si son el mismo entonces ambos tiempos son 0
+        if (P == H) tiempo_hasta_H = tiempo_hasta_P = 0;
 
-        // cout << "Proxima manifestacion para H: " << proxima_manifestacion.nodo << ", " << proxima_manifestacion.tiempo << endl;
-
-        camino_P = calcular_camino_hasta_P();
-        /*
-        // Mostramos los marcados
-        cout << "Marcados post H:" << endl;
-        for(int i = 0; i < n*m; i++)
-            cout << "v: " << i << ", m:" << marcados[i] << endl;
-
-        cout << "Caminos desde H post calcular:" << endl;
-        for(int i = 0; i < n*m; i++)
-            cout << "v: " << i << ", t:" << tiempo_H[i] << endl;
-        */
-        // Calculamos el camino a H
-        bfs(P, tiempo_P);
-        /*
-        // Mostramos los caminos desde P
-        cout << "Caminos desde P antes de calcular:" << endl;
-        for(int i = 0; i < n*m; i++)
-            cout << "v: " << i << ", t:" << tiempo_P[i] << endl;
-        */
-        proxima_manifestacion = proxima_manifestacion_para(tiempo_P[H]);
-        
-        // cout << "Proxima manifestacion para P: " << proxima_manifestacion.nodo << ", " << proxima_manifestacion.tiempo << endl;
-
-        camino_H = calcular_camino_hasta_H();
-        /*
-        cout << "Marcados post P:" << endl;
-        for(int i = 0; i < n*m; i++)
-            cout << "v: " << i << ", m:" << marcados[i] << endl;
-
-        cout << "Caminos desde P despues de calcular:" << endl;
-        for(int i = 0; i < n*m; i++)
-            cout << "v: " << i << ", t:" << tiempo_P[i] << endl;
-        */
-
-        if (camino_P == -1 or camino_H == -1) cout << IMPOSIBLE << endl;
-        else cout << camino_P << " " << camino_H + camino_P << endl;
+        // Imprimimos el resultado
+        if (tiempo_hasta_H == -1 or tiempo_hasta_P == -1) cout << IMPOSIBLE << endl;
+        else cout << tiempo_hasta_P << " " << tiempo_hasta_H + tiempo_hasta_P << endl;
 
         manifestaciones.clear();
-        // cout << " --- " << endl;
     }
 }
 
